7f6b475f8cb28378e71c7d4dee85061c8fbfdc7c
musadhiq <musadhiqmp@gmail.com>
2023-04-03 12:51:26 +0530

    cors added

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var bodyParser = require("body-parser");
-
+var cors = require("cors");
 var adminRouter = require("./routes/admin");
 var usersRouter = require("./routes/user");
 
@@ -21,6 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "uploads")));
+app.use(cors());
 //app.use(express.static('uploads'))
 app.use("/", express.static("uploads"));
 
